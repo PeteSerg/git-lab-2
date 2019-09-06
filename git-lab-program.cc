35043d5f1893f0fa5837e6e0f61cf71641888030
Pete_Sergakis <peteserg@gmail.com>
2019-09-06 17:09:56 -0400

    test for git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *          File: git-lab-program.cc
+ *        Author: Pete sergakis
+ *          Date: 9/6/2019
+ *    Decription: Test program for git-lab
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *arg[]) {
+     
+     cout << "Hello Git!!" << endl;
+
+     return 0;
+ }//main
